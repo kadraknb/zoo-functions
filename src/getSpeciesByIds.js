f65a3a44a8c17c2529f948d3cdcc97f3c72ade64
kadraknb <kadrak02@hotmail.com>
2022-05-26 20:55:49 -0300

    Requisitos 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
+/* eslint-disable no-multiple-empty-lines */
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const get = [];
+  ids.forEach((id) => {
+    get.push(data.species.find((aa) => aa.id === id));
+  });
+  return get;
 }
+
+
+// return data.species.filter((bb, cc) => bb.id === ids[cc]);
+
+// let filtro = (ids) => data.species.filter((bb) => bb.id === ids);
+
+
+// console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
+
+
+
 
 module.exports = getSpeciesByIds;
