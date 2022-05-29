fd81d20b6dfc96e99ddbe0272034481157899888
kadraknb <kadrak02@hotmail.com>
2022-05-29 15:11:24 -0300

    Requisitos 6,7,15

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,19 +1,12 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  let animais = {};
-  // Object.keys.length
+  const animais = {};
   const anima = () => data.species.find((aa) => aa.name === animal.specie);
   const quantidade = () => anima().residents.length;
   const quantidadeSex = () => anima().residents.filter((aa) => aa.sex === animal.sex).length;
-  if (animal !== undefined) {
-    animais = animal.sex !== undefined ? quantidadeSex() : quantidade();
-  } else {
-    data.species.forEach((aa) => { animais[aa.name] = aa.residents.length; });
-  }
+  if (animal !== undefined) { return animal.sex !== undefined ? quantidadeSex() : quantidade(); }
+  data.species.forEach((aa) => { animais[aa.name] = aa.residents.length; });
   return animais;
 }
-// console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
-// console.log(countAnimals({ specie: 'penguins' }));
-console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,43 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('se nao receber parametro', () => {
+    const actual = handlerElephants();
+
+    expect(actual).toBeUndefined();
+  });
+  it('se nao receber um string', () => {
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    const actual = handlerElephants(3);
+
+    expect(actual).toStrictEqual(expected);
+  });
+  it('verificar se passa informa;cao do helefante', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+
+    expect(actual).toBe(expected);
+  });
+  it('verificar se passa informa;cao de todos as quantidades de elefantes', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+
+    expect(actual).toStrictEqual(expected);
+  });
+  it('verificar se passa informa;cao de todos os nomes de elefantes', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+
+    expect(actual).toEqual(expected);
+  });
+  it('verificar se passa informa;cao da mediade de idade de todos oselefantes', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+
+    expect(actual).toBe(expected);
+  });
+  it('verificar se passa nulo', () => {
+    const actual = handlerElephants('seila');
+    expect(actual).toBeNull();
+  });
+});
