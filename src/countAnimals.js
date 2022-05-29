ad175be72c73ac57a6eb044138a7a45e83af6e0b
kadraknb <kadrak02@hotmail.com>
2022-05-29 05:20:36 -0300

    Requisitos 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let animais = {};
+  // Object.keys.length
+  const anima = () => data.species.find((aa) => aa.name === animal.specie);
+  const quantidade = () => anima().residents.length;
+  const quantidadeSex = () => anima().residents.filter((aa) => aa.sex === animal.sex).length;
+  if (animal !== undefined) {
+    animais = animal.sex !== undefined ? quantidadeSex() : quantidade();
+  } else {
+    data.species.forEach((aa) => { animais[aa.name] = aa.residents.length; });
+  }
+  return animais;
 }
-
+// console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
+// console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 module.exports = countAnimals;
