b92de2ccb2c54609fc54c41d85c193aadcae8a26
kadraknb <kadrak02@hotmail.com>
2022-05-30 08:47:48 -0300

    Requisitos 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function countEntrants(aa) {
+  const chi = aa.filter((bb) => bb.age < 6).length;
+  const sen = aa.filter((bb) => bb.age > 49).length;
+  const adu = aa.filter((bb) => bb.age > 6 && bb.age < 49).length;
+  return { child: chi, adult: adu, senior: sen };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(aa = {}) {
+  if (typeof aa[0] === 'object') {
+    let valor = 0;
+    const per = Object.values(data.prices);
+    const preço = [per[2], per[0], per[1]];
+    Object.values(countEntrants(aa)).forEach((bb, cc) => { valor += bb * preço[cc]; });
+    return valor;
+  }
+  return 0;
 }
 module.exports = { calculateEntry, countEntrants };
