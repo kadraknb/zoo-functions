fb4a35b63a480daa70bd2cc9927290a1b4318898
kadraknb <kadrak02@hotmail.com>
2022-05-31 23:57:47 -0300

    Requisitos 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(zz) {
+  const sei = (yy) => yy.map((xx) => {
+    const noLo = [[], []];
+    const animal = (aa) => aa.responsibleFor.map((bb) => data.species.find((cc) => bb === cc.id));
+    const res = (id, fullName, species, locations) => ({ id, fullName, species, locations });
+    animal(xx).forEach((aa) => { noLo[0].push(aa.name); noLo[1].push(aa.location); });
+    return res(xx.id, `${xx.firstName} ${xx.lastName}`, noLo[0], noLo[1]);
+  });
+  if (zz !== undefined) {
+    const zzArr = (aa) => Object.values(aa)[0];
+    const ele = data.employees.filter((aa) => Object.values((aa)).slice(0, 3).includes(zzArr(zz)));
+    if (ele[0] === undefined) { throw new Error('Informações inválidas'); }
+    try { return sei(ele)[0]; } catch (error) { throw error.message; }
+  }
+  return sei(data.employees);
 }
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,9 +2,9 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const prispecie = data.employees.find((aa) => aa.id === id).responsibleFor[0];
-  const priAnimal = data.species.find((aa) => aa.id === prispecie).residents;
-  const idade = priAnimal.map((aa) => aa.age).sort((a, b) => a - b).slice(-1)[0];
-  return Object.values(priAnimal.filter((aa) => aa.age === idade)[0]);
+  const animais = data.species.find((aa) => aa.id === prispecie).residents;
+  const idade = animais.map((aa) => aa.age).sort((a, b) => a - b).slice(-1)[0];
+  return Object.values(animais.filter((aa) => aa.age === idade)[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
