989efd080efe0af6b3aa83ec5609d2ac6f8d73db
kadraknb <kadrak02@hotmail.com>
2022-06-01 16:06:45 -0300

    Requisitos 12,13,16

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -6,6 +6,7 @@ function getSchedule(zz) {
   const horas = Object.values(data.hours);
   const semana = Object.keys(data.hours);
   const mon = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+
   const week = (bb) => `Open from ${horas[bb].open}am until ${horas[bb].close}pm`;
   const segunda = () => { res.Monday = mon; return res; };
   const animalDias = (animal) => species.find((aa) => aa.name === animal).availability;
@@ -13,6 +14,7 @@ function getSchedule(zz) {
   const dia = (b) => ({ exhibition: an(b), officeHour: week(semana.findIndex((aa) => aa === b)) });
   const table = (aa, bb) => { res[aa] = { officeHour: week(bb), exhibition: an(aa) }; return res; };
   const noParame = () => { semana.forEach((aa, bb) => { table(aa, bb); }); segunda(); return res; };
+
   if (zz === 'Monday') { return segunda(); }
   if (species.some((aa) => aa.name === zz)) { return animalDias(zz); }
   if (semana.includes(zz)) { res[zz] = dia(zz); return res; }
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,72 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('se sem parametro retorna um tabela com os horarios ', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+
+    expect(actual).toEqual(expected);
+  });
+  it('se o primeiro parametro for segunda o retorno vai ser um erro ', () => {
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+
+    expect(actual).toBe(expected);
+  });
+  it('se for pasado um horai que esta aberto, retorna um mensagem com o aviso', () => {
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+
+    expect(actual).toBe(expected);
+  });
+  it('se for pasado um horai que esta fechado, retorna um mensagem com o aviso', () => {
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is closed';
+
+    expect(actual).toBe(expected);
+  });
+  it('se o segundo parametro estiver com os minutos fora do padrao, retornara um erro', () => {
+    const actual = () => getOpeningHours('Wednesday', '09:0g-PM');
+    const expected = 'The minutes should represent a number';
+
+    expect(actual).toThrow(expected);
+  });
+  it('se o segundo parametro estiver com as horas fora do padrao, retornara um erro', () => {
+    const actual = () => getOpeningHours('Wednesday', '0g:00-PM');
+    const expected = 'The hour should represent a number';
+
+    expect(actual).toThrow(expected);
+  });
+  it('se o segundo parametro estiver fora do padrao, retornara um erro', () => {
+    const actual = () => getOpeningHours('Monday', '09:00-bM');
+    const expected = 'The abbreviation must be \'AM\' or \'PM\'';
+
+    expect(actual).toThrow(expected);
+  });
+  it('se o segundo parametro estiver com a hora maior que 12, retornara um erro', () => {
+    const actual = () => getOpeningHours('Monday', '13:00-PM');
+    const expected = 'The hour must be between 0 and 12';
+
+    expect(actual).toThrow(expected);
+  });
+  it('se o segundo parametro estiver com os minutos maior que 60, retornara um erro', () => {
+    const actual = () => getOpeningHours('Monday', '10:70-PM');
+    const expected = 'The minutes must be between 0 and 59';
+
+    expect(actual).toThrow(expected);
+  });
+  it('se o primeiro parametro nao estiver com um dia da semana, retornara um erro', () => {
+    const actual = () => getOpeningHours('seila', '10:00-PM');
+    const expected = 'The day must be valid. Example: Monday';
+
+    expect(actual).toThrow(expected);
+  });
+});
